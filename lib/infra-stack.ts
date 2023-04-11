98faa60dd894c1b5dcb189e1dab6e829cea4fd47
Fabrice Foagoue <foagoue_fabrice@bah.com>
2023-04-11 13:48:14 -0400

    init

--- lib/infra-stack.ts
+++ lib/infra-stack.ts
@@ -21,6 +21,7 @@ export class InfraStack extends cdk.Stack {
         name: "demosubnet",
         subnetType: ec2.SubnetType.PUBLIC,
         }
+
       ]
     }) 
     // Allow SSH (TCP Port 22) and HTTP port 80 access 
@@ -49,12 +50,11 @@ export class InfraStack extends cdk.Stack {
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T4G, ec2.InstanceSize.MICRO),
       machineImage: ami,
       securityGroup: securityGroup,
-      //keyName: key.keyPairName,
       role: role
       
     });
 
-    // Create an asset that will be used as part of User Data to run on first load
+    // Create an asset for userdata
     const asset = new Asset(this, 'Asset', { path: path.join(__dirname, '../../service/config.sh') });
     const localPath = ec2Instance.userData.addS3DownloadCommand({
       bucket: asset.bucket,
@@ -69,10 +69,6 @@ export class InfraStack extends cdk.Stack {
 
     // Create outputs for connecting
     new cdk.CfnOutput(this, 'IP Address', { value: ec2Instance.instancePublicIp });
-    // new cdk.CfnOutput(this, 'Key Name', { value: key.keyPairName })
-    new cdk.CfnOutput(this, 'Download Key Command', { value: 'aws secretsmanager get-secret-value --secret-id ec2-ssh-key/cdk-keypair/private --query SecretString --output text > infra/cdk-key.pem && chmod 400 infra/cdk-key.pem' })
-    new cdk.CfnOutput(this, 'ssh command', { value: 'ssh -i cdk-key.pem -o IdentitiesOnly=yes ec2-user@' + ec2Instance.instancePublicIp })
-  
   }
 }
 
